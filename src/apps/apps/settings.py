ad1eacf54d5a4cfead7bb7f02bb605b25c19f1c8
abbas <abbasas690@gmail.com>
2024-08-24 18:24:00 +0530

    added env file

--- src/apps/apps/settings.py
+++ src/apps/apps/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+y!9w+w66ayxg2$&extv65g_5_(g0c)qmtkedhoj9n*-(a4j0&'
-
+# SECRET_KEY = 'django-insecure-+y!9w+w66ayxg2$&extv65g_5_(g0c)qmtkedhoj9n*-(a4j0&'
+SECRET_KEY=config("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG") or True
+# DEBUG = os.environ.get("DJANGO_DEBUG") or True
+DEBUG=config("DJANGO_DEBUG",cast=bool)
 
 ALLOWED_HOSTS = [
     ".railway.app"
